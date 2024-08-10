f8c51f34320cf6d1ae7480623b9bb31272714aff
ashenshanuka <shanuka-im19008@stu.kln.ac.lk>
2024-08-10 23:33:10 +0530

    first commit

---
+++ src/WhacAMole.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import javax.swing.*;
+
+ 
+public class WhacAMole {
+    int boardWidth = 600;
+    int boardHeight = 650;
+    
+    JFrame frame=new JFrame("Mario : Whac a Mole");
+    JLabel textLabel =new JLabel();
+    JPanel textPanel= new JPanel();
+    JPanel boardJPanel= new JPanel();
+
+    WhacAMole() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setFont(new Font("Arial",Font.PLAIN,50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Score : 0");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel,BorderLayout.NORTH);
+
+
+    }
+
+    
+}
